d1be826a62ec15ee642c8dddd80368d29c455458
Morgana <zhuh4574@gmail.com>
2023-06-08 22:39:19 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Header from "./components/layout/Header";
+import Meals from "./components/Meals/Meals";
+
+function App() {
+  return (
+    <div>
+      <Header/>
+      <main>
+        <Meals></Meals>
+      </main>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Meals/Meals.js
@@ -0,0 +1,11 @@
+import MealsSummary  from "./MealsSummary/MealsSummary"
+import AvailableMeals from "./AvailableMeals/AvailableMeals"
+
+const Meals = () =>{
+    return <div>
+        <MealsSummary></MealsSummary>
+        <AvailableMeals></AvailableMeals>
+    </div>
+}
+
+export default Meals 
